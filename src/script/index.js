1e0c72d4225d684a6d625d8df41580fe3a3773b7
Anastasia Pirogova <120117437+pirogovairina12@users.noreply.github.com>
2023-05-23 00:51:46 +0300

    Update index.js

--- src/script/index.js
+++ src/script/index.js
@@ -117,14 +117,3 @@ function initializeLightbox() {
     captionDelay: 250,
   });
 }
-
-
-
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
-//
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: 'smooth',
-// });
